661d8be2114538ddae1354f99a4ccd339e3d7917
inwoong32 <14soobean@gmail.com>
2024-08-05 01:47:44 +0900

    update

--- src/trajectory_selector.cpp
+++ src/trajectory_selector.cpp
@@ -9,7 +9,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <autoware_planning_msgs/msg/lanelet_route.hpp>
-#include <avante_msgs/msg/lane_change_check.hpp>
+#include <autoware_planning_msgs/msg/lane_change_check.hpp>
 #include <mutex>
 
 class TrajectorySelector : public rclcpp::Node
@@ -25,14 +25,14 @@ public:
     pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>( "/localization/pose_estimator/pose", rclcpp::QoS{1}, std::bind(&TrajectorySelector::callbackPose, this, std::placeholders::_1));
     route_pub_= create_publisher<autoware_planning_msgs::msg::LaneletRoute>("/planning/mission_planning/route", 
                                                                             rclcpp::QoS{1}.transient_local());
-    lane_change_check_sub_= create_subscription<avante_msgs::msg::LaneChangeCheck>("/planning/lane_change_check", rclcpp::QoS{1}, std::bind(&TrajectorySelector::callbackLaneChangeCheck, this, std::placeholders::_1));
+    lane_change_check_sub_= create_subscription<autoware_planning_msgs::msg::LaneChangeCheck>("/planning/lane_change_check", rclcpp::QoS{1}, std::bind(&TrajectorySelector::callbackLaneChangeCheck, this, std::placeholders::_1));
   }
   autoware_planning_msgs::msg::LaneletRoute TR_route_msg_;
-  avante_msgs::msg::LaneChangeCheck lccMsg;
+  autoware_planning_msgs::msg::LaneChangeCheck lccMsg;
   rclcpp::Subscription<autoware_planning_msgs::msg::LaneletRoute>::ConstSharedPtr TR_route_sub_;
   rclcpp::Subscription<autoware_planning_msgs::msg::LaneletRoute>::ConstSharedPtr LC_route_sub_;
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
-  rclcpp::Subscription<avante_msgs::msg::LaneChangeCheck>::SharedPtr lane_change_check_sub_;
+  rclcpp::Subscription<autoware_planning_msgs::msg::LaneChangeCheck>::SharedPtr lane_change_check_sub_;
   rclcpp::Publisher<autoware_planning_msgs::msg::LaneletRoute>::SharedPtr route_pub_;
 private:
   void TRroutecallback(const autoware_planning_msgs::msg::LaneletRoute::ConstSharedPtr msg)
@@ -51,7 +51,7 @@ private:
 
   // lane change 여부를 판단하는 토픽을 subscribe한다. 
   // lock을 이용해야 한다.
-  void callbackLaneChangeCheck(const avante_msgs::msg::LaneChangeCheck msg)
+  void callbackLaneChangeCheck(const autoware_planning_msgs::msg::LaneChangeCheck msg)
   {
     lccMsg = msg;
   }
